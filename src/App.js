3b4e22fc878740f1483cf8e5b5d1cdc1e2c22316
daniel70125 <daniel.wright@ad.infosys.com>
2022-05-13 21:09:44 -0500

    ''

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import './App.css';
+import Routes from './routes';
+import {connect} from 'react-redux';
+import {getUser} from './Redux/Reducer';
+import axios from 'axios';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  async componentDidMount(){
+    await axios.get('/getuser')
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
+    // this.props.getUser();
+    // await axios.get('/session')
+    // .then(res => this.props.getUser(res.data))
+    // .catch(err => console.log(err))
+  }
+  render() { 
+    console.log(this.props);
+    return ( 
+      <div className="App">
+        {Routes}
+      </div>
+     );
+  }
+}
+const mapStateToProps = reduxState => reduxState;
+export default connect(mapStateToProps, {getUser})(App);
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from '../Header/Header';
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <Header />
+                <h1>Home.js</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Home;
